4c237c4e97cabb605d22956e49f22446c81f85f2
arisgk <aris.gk@gmail.com>
2017-09-06 15:08:30 +0300

    Added Home route

--- src/components/Routes/index.jsx
+++ src/components/Routes/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Route } from 'react-router';
+import Home from '../Home';
 
 const Routes = () => (
-  <div>Hello World!</div>
+  <Route exact path="/" component={Home} />
 );
 
 export default Routes;
