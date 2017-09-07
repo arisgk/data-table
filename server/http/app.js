3eb2ffe23cb93410a31d26c19328bc79a7729bbd
arisgk <aris.gk@gmail.com>
2017-09-07 13:29:49 +0300

    Add support for client-side routing

--- server/http/app.js
+++ server/http/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const graphqlRoute = require('./routes/graphql');
 const errorRoute = require('./routes/error');
@@ -6,9 +7,17 @@ const errorRoute = require('./routes/error');
 const app = express();
 
 module.exports = (services) => {
-  const graphql = graphqlRoute.create(services);
   app.use(bodyParser.json());
+  app.use(express.static(path.join(__dirname, 'build')));
+
+  const graphql = graphqlRoute.create(services);
+
   app.use('/graphql', graphql);
   app.use(errorRoute);
+
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+
   return app;
 };
