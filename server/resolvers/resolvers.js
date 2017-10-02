1147a92269aa375cc343c9ecd9b9ac426fd7a09f
arisgk <aris.gk@gmail.com>
2017-10-02 16:40:36 +0300

    Update resolvers export

--- server/resolvers/resolvers.js
+++ server/resolvers/resolvers.js
@@ -1,15 +1,11 @@
-function create({ userRepository }) {
-  return {
-    Query: {
-      users: async () => userRepository.readAll(),
+module.exports = ({ userRepository }) => ({
+  Query: {
+    users: async () => userRepository.readAll(),
+  },
+  Mutation: {
+    addUser: async (root, { firstName, lastName, age }) => {
+      const user = await userRepository.insert({ firstName, lastName, age });
+      return user;
     },
-    Mutation: {
-      addUser: async (root, { firstName, lastName, age }) => {
-        const user = await userRepository.insert({ firstName, lastName, age });
-        return user;
-      },
-    },
-  };
-}
-
-module.exports = create;
+  },
+});
